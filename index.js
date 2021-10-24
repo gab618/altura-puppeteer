7f2a2078795fd6e08c387896fba9a80aff838a04
Gabriel <knuckles4x@gmail.com>
2021-10-24 16:37:42 -0300

    get celebrity height

--- index.js
+++ index.js
@@ -11,14 +11,54 @@ const puppeteer = require("puppeteer-core");
   const options = {
     args: [],
     executablePath: exePath,
-    headless: false,
-    slowMo: 250,
+    headless: true,
+    // slowMo: 250,
   };
 
   const browser = await puppeteer.launch(options);
 
-  //start
-  const page = await browser.newPage();
-  await page.goto("https://google.com");
-  // await browser.close();
+  async function getDataByName(name) {
+    const page = await browser.newPage();
+    await page.goto("https://google.com");
+    await page.waitForSelector('[title="Pesquisar"]');
+    await page.type('[title="Pesquisar"]', `${name} altura`);
+    await page.keyboard.press("Enter");
+
+    try {
+      await page.waitForSelector('[data-attrid="title"]', { timeout: 2000 });
+
+      const celebrityData = await page.evaluate(() => {
+        const name = document.querySelector(
+          '[data-attrid="title"] > span'
+        ).textContent;
+        const subtitle = document.querySelector(
+          '[data-attrid="subtitle"] > span'
+        ).textContent;
+        const heigth = Number(
+          document
+            .querySelector('[data-attrid="kc:/people/person:height"] > div')
+            .textContent.replace(",", ".")
+            .replace("m", "")
+            .trim()
+        );
+        return { name, subtitle, heigth };
+      });
+      console.log(celebrityData);
+      await page.close();
+    } catch (error) {
+      if (error instanceof puppeteer.errors.TimeoutError) {
+        console.log(`${name} not found in Google`);
+        await page.close();
+        return;
+      }
+    }
+  }
+
+  await getDataByName("Michael Jackson");
+  await getDataByName("Michael Jordan");
+  await getDataByName("Lula");
+  await getDataByName("Jair Bolsonaro");
+  await getDataByName("Silvio Santos");
+
+  await browser.close();
 })();
